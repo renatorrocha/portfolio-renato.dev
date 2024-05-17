14ee05b18402db2cbe16d98809bfb32768c32da1
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-16 22:19:14 -0300

    add underConstruction component and some styles

---
+++ src/components/under-construction.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export default function UnderConstruction() {
+  const Links = [
+    { label: "github", url: "https://github.com/renatorrocha" },
+    {
+      label: "linkedin",
+      url: "https://www.linkedin.com/in/renato-rrodrigues/",
+    },
+  ];
+
+  return (
+    <main className="items-center justify-center p-4 flex flex-col gap-4">
+      <p className="font-bold">Em construção...</p>
+
+      <div className="flex justify-between gap-4">
+        {Links.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className="hover:font-semibold transition-all duration-300 hover:scale-105"
+          >
+            <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
